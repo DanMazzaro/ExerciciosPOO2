9fe2a7b914562bd8c696ad49ad91ab15e2a0690c
DanMazzaro <danielmazzaro44@gmail.com>
2025-09-18 08:56:28 -0300

    Add files via upload

---
+++ Cachorro.py
@@ -0,0 +1,15 @@
+from Animal import Animal
+
+class Cachorro(Animal):
+
+    def __init__(self, nome, idade, raca):
+        super().__init__(self, nome, idade)
+        self._raca = raca
+
+    def fazer_som(self):
+        return f'{self._nome} late: Au Au'
+    
+    def buscar_bolinha(self):
+        return f'{self._nome} esta buscando a bolinha'
+    
+
